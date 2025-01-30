79514f6faf6fea8702b043d1d7fd1db508956ded
JackGoulding111 <jackgoulding34@gmail.com>
2025-01-30 10:19:56 +0000

    commit 1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const renderButton = (i) => {
+    return <button className="grid-button" key={i}>_</button>;
+  };
+
+  const renderGrid = () => {
+    let grid = [];
+    for (let row = 0; row < 3; row++) {
+      let buttons = [];
+      for (let col = 0; col < 3; col++) {
+        buttons.push(renderButton(row * 3 + col));
+      }
+      grid.push(<div className="grid-row" key={row}>{buttons}</div>);
+    }
+    return grid;
+  };
+
+  return (
+    <div className="App">
+      <div className="grid-container">
+        {renderGrid()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
